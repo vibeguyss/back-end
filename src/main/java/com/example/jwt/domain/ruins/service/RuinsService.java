1e237c494a2c5caa3fb985d00d6b6f7e6f96087b
ENCAIL <siwon0876@gmail.com>
2025-06-09 09:38:18 +0900

    add serch ruins

--- src/main/java/com/example/jwt/domain/ruins/service/RuinsService.java
+++ src/main/java/com/example/jwt/domain/ruins/service/RuinsService.java
@@ -1,4 +1,21 @@
 package com.example.jwt.domain.ruins.service;
 
+import com.example.jwt.domain.ruins.model.repo.RuinsJpaRepo;
+import com.example.jwt.domain.ruins.presentation.dto.RuinsRes;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class RuinsService {
+    private final RuinsJpaRepo ruinsJpaRepo;
+
+    public List<RuinsRes> getRuinsInMapBounds(BigDecimal minLat, BigDecimal maxLat, BigDecimal minLng, BigDecimal maxLng) {
+        return ruinsJpaRepo.findInBounds(minLat, maxLat, minLng, maxLng).stream()
+                .map(RuinsRes::from)
+                .toList();
+    }
 }
