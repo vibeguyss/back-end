e4a341df117b562d8b613544a6fe42ca628cd4d6
ganggun <165283835+ganggun@users.noreply.github.com>
2025-07-13 23:24:22 +0900

    fix: token not exist error

--- src/main/java/com/learnmore/legacy/global/security/jwt/error/JwtError.java
+++ src/main/java/com/learnmore/legacy/global/security/jwt/error/JwtError.java
@@ -15,7 +15,8 @@ public enum JwtError implements CustomError {
     INVALID_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰 타입입니다."),
     INCORRECT_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED,"존제하지 않는 리프레쉬 토큰입니다. 로그인 해주세요 "),
-    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED,"만료된 토큰입니다");
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED,"만료된 토큰입니다."),
+    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED,"토큰이 필요한 접근입니다.");
 
     private final HttpStatus status;
     private final String message;
